f873b4d0f2507753fbaecec5940ff88616dfbd9c
Sanja Jasek <jasek.sanja@gmail.com>
2024-01-10 12:16:33 +0100

    number of postynapses: Calculate average and median numbers of postsynaptic sites

--- code/number_of_postsynaptic_sites_per_synapse.R
+++ code/number_of_postsynaptic_sites_per_synapse.R
@@ -1,11 +1,11 @@
-
 library(catmaid)
+library(data.table)
 
 conn <- source("~/R/conn.R")
 
 
 pre_connectors <- catmaid_fetch(
-  path = "35/connectors/links",
+  path = "35/connectors/",
   body = list(
     relation_type = "presynaptic_to",
     with_partners = "false"
@@ -16,8 +16,14 @@ n_pre_post <- function(connector_id) {
   connector_info <- catmaid_fetch(path = paste("35/connectors/", connector_id, sep = ""))
   # pre should always be 1, but maybe it's good to check if there is any weird stuff going on
   pre <- connector_info$partners %like% 'presynaptic_to'
-  n_pre <- table(pre)["TRUE"]
+  n_pre <- table(pre)["TRUE"][[1]]
   post <- connector_info$partners %like% 'postsynaptic_to'
-  n_post <- table(post)["TRUE"]
+  n_post <- table(post)["TRUE"][[1]]
   return(n_post)
 }
+
+pre_connector_IDs <- lapply(pre_connectors$connectors,'[[',1)
+
+n_post <- lapply(pre_connector_IDs, n_pre_post)
+
+summary(as.numeric(n_post))
