8d6c2974000c3d0b196e4c68e04042e4851a5c8b
Sanja Jasek <jasek.sanja@gmail.com>
2024-01-22 14:14:50 +0100

    cilium lengths: fix variable, correct typos
    Lengths of biciliated cells were calculated incorrectly because
    wrong variable was used.

--- code/cilium_lengths.R
+++ code/cilium_lengths.R
@@ -20,27 +20,27 @@ cilium_length <- function(ctip_id) {
   path <- paste("/35/treenodes/", ctip_id, "/compact-detail", sep = "")
   ctip_treenode <- catmaid_fetch(path = path)
   skid <- ctip_treenode[[8]]
-  neuron <- read.neuron.catmaid(skid, pid = 35)
+  neuron_info <- read.neuron.catmaid(skid, pid = 35)
   # check which treenodes have basal body annotation, because this is the stop point
-  basalb_ids <- neuron$tags$`basal body`
+  basalb_ids <- neuron_info$tags$`basal body`
   treenode_id <- ctip_id
   distance <- 0
   while (!(treenode_id %in% basalb_ids)) {
-    index <- match(treenode_id, neuron$d$PointNo)
-    parent_id <- neuron$d$Parent[[index]]
+    index <- match(treenode_id, neuron_info$d$PointNo)
+    parent_id <- neuron_info$d$Parent[[index]]
     if (parent_id == -1) {
-      print("root reached before basal body tag, from ciium tip")
+      print("root reached before basal body tag, from cilium tip")
       print(ctip_id)
       stop()
     }
-    index_parent <- match(parent_id, neuron$d$PointNo)
+    index_parent <- match(parent_id, neuron_info$d$PointNo)
  
-    distance_2nodes <- dist(rbind(c(neuron$d$X[[index]],
-                                neuron$d$Y[[index]],
-                                neuron$d$Z[[index]]),
-                              c(neuron$d$X[[index_parent]],
-                                neuron$d$Y[[index_parent]],
-                                neuron$d$Z[[index_parent]])
+    distance_2nodes <- dist(rbind(c(neuron_info$d$X[[index]],
+                                neuron_info$d$Y[[index]],
+                                neuron_info$d$Z[[index]]),
+                              c(neuron_info$d$X[[index_parent]],
+                                neuron_info$d$Y[[index_parent]],
+                                neuron_info$d$Z[[index_parent]])
                                 ))
     distance <- distance + distance_2nodes
     #print(distance)
@@ -49,21 +49,21 @@ cilium_length <- function(ctip_id) {
   return(c(distance, length(basalb_ids)))
 }
 
-cilium_lenghts_monociliated <- list()
-cilium_lenghts_biciliated <- list()
+cilium_lengths_monociliated <- list()
+cilium_lengths_biciliated <- list()
 for (ctip_id in ctips) {
   c_length_type <- c(cilium_length(ctip_id))
   cilium_length_new <- c_length_type[[1]]
   nbb <- c_length_type[[2]]
   names(cilium_length_new) <- ctip_id
   if (nbb == 1) {
-    cilium_lenghts_monociliated <- c(cilium_lenghts_monociliated, cilium_length_new)
+    cilium_lengths_monociliated <- c(cilium_lengths_monociliated, cilium_length_new)
   }
   if (nbb == 2) {
-    cilium_lenghts_biciliated <- c(cilium_lenghts_monociliated, cilium_length_new)
+    cilium_lengths_biciliated <- c(cilium_lengths_biciliated, cilium_length_new)
   }
 }
 
-hist(as.numeric(cilium_lenghts_monociliated))
-hist(as.numeric(cilium_lenghts_biciliated))
-t.test(as.numeric(cilium_lenghts_monociliated), as.numeric(cilium_lenghts_biciliated))
+hist(as.numeric(cilium_lengths_monociliated))
+hist(as.numeric(cilium_lengths_biciliated))
+t.test(as.numeric(cilium_lengths_monociliated), as.numeric(cilium_lengths_biciliated))
