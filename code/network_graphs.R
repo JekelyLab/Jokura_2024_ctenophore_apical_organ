800fa2fd3ac4ad6364a5e079b4344a2d80e5c7fc
Gaspar Jekely <g.jekely@exeter.ac.uk>
2024-02-22 12:04:14 +0100

    network:add more celltypes

--- code/network_graphs.R
+++ code/network_graphs.R
@@ -48,6 +48,40 @@ syn_neuron <- nlapply(
   function(x) smooth_neuron(x, sigma = 1000)
 )
 
+bridge <- nlapply(
+  read.neurons.catmaid(
+    "celltype:bridge", pid = 35
+  ),
+  function(x) smooth_neuron(x, sigma = 1000)
+)
+
+# cell types
+AO_celltypes <- list(balancer, LB, syn_neuron, bridge)
+AO_celltype_names <- list(
+  "balancer", "LB", "syn_neuron", "bridge"
+  )
+
+# iterate through cell group neuron lists and get connectivity
+# define empty synapse list with the right dimensions
+synapse_list <- c()
+
+for (i in 1:length(AO_celltypes)) {
+  for (j in 1:length(AO_celltypes)) {
+    # get connectors between two cell groups
+    presyn_skids <- attr(AO_celltypes[i][[1]], "df")$skid
+    postsyn_skids <- attr(AO_celltypes[j][[1]], "df")$skid
+    connectivity <- catmaid_get_connectors_between(
+      pre = presyn_skids,
+      post = postsyn_skids, pid = 35
+    )
+    # check the number of synapses from group1 -> group2
+    N_synapses <- dim(connectivity)[1]
+    if(length(connectivity) == 0) {N_synapses = 0}
+    # add value to synapse list
+    synapse_list <- c(synapse_list, N_synapses)
+  }
+}
+synapse_list
 
 # create graph with cell types as nodes
 node_IDs <- data.frame(name = cell_types)
