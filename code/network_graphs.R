62d28a986016d57989c773e25715825ec51c8122
Gaspar Jekely <g.jekely@exeter.ac.uk>
2024-02-22 12:18:37 +0100

    network: convert to visnetwork

--- code/network_graphs.R
+++ code/network_graphs.R
@@ -102,7 +102,86 @@ rownames(synapse_matrix) <- as.character(AO_celltype_names)
 colnames(synapse_matrix) <- as.character(AO_celltype_names)
 synapse_matrix
 
+# with the make_graph function of igraph we turn it into a graph (input is the list of edge pairs)
+AO_graph <- graph_from_adjacency_matrix(
+  synapse_matrix,
+  mode = c("directed"),
+  weighted = TRUE, diag = TRUE
+)
 
+AO_graph |> as_tbl_graph() 
+
+
+# calculate node weighted degree -------------
+degree=degree(
+  AO_graph, v = V(AO_graph), mode = c("all"), 
+  loops = TRUE, normalized = FALSE
+  )
+degree
+
+# use visNetwork to plot the network --------------------------------------
+
+## convert to VisNetwork-list
+AO_graph.visn <- toVisNetworkData(AO_graph)
+
+## copy column "weight" to new column "value" in list "edges"
+Conn_graph.visn$edges$value <- Conn_graph.visn$edges$weight
+Conn_graph.visn$nodes$value = degree
+Conn_graph.visn$nodes$group <- cell_group_attr$type
+
+
+#hierarchical layout
+
+#level	: Number. Default to undefined. When using the hierarchical layout, the level determines where the node is going to be positioned.
+Conn_graph.visn$nodes$level <- cell_group_attr$level
+#hierarchical layout
+{
+  visNet <- visNetwork(Conn_graph.visn$nodes,Conn_graph.visn$edges) %>% 
+    visIgraphLayout(layout = "layout_nicely", physics = TRUE, 
+                    randomSeed = 42, type="square") %>%
+    visHierarchicalLayout(levelSeparation=250, 
+                          nodeSpacing=10,
+                          direction='LR',
+                          sortMethod='hubsize',
+                          shakeTowards='roots') %>%
+    visEdges(smooth = list(type = 'curvedCW', roundness=0.1),
+             scaling=list(min=2, max=12),
+             color = list(inherit=TRUE, opacity=0.7),
+             arrows = list(to = list(enabled = TRUE, 
+                                     scaleFactor = 1.2, type = 'arrow'))) %>%
+    visNodes(borderWidth=0.3, 
+             color = list(background=Conn_graph.visn$nodes$color, border='black'),
+             opacity=0.9,
+             shape='dot', 
+             font=list(color='black', size=44),
+             scaling = list(label=list(enabled=TRUE, min=48, max=56)),
+             level= Conn_graph.visn$nodes$level) %>%
+    visOptions(highlightNearest = list(enabled=TRUE, degree=1, algorithm='hierarchical',labelOnly=FALSE)) %>%
+    visInteraction(dragNodes = TRUE, dragView = TRUE,
+                   zoomView = TRUE, hover=TRUE,
+                   multiselect=TRUE) %>%
+    visGroups(groupname = "MBSN", shape = "square", 
+              opacity=1, color="#E69F00") %>%
+    visGroups(groupname = "MBintrIN", shape = "square", 
+              opacity=1, color="#CC79A7") %>%
+    visGroups(groupname = "MBON", shape = "square", 
+              opacity=1, color="#56B4E9") %>%
+    visGroups(groupname = "IN", shape = "dot", 
+              opacity=1, color="#0072B2") %>%
+    visGroups(groupname = "SN", shape = "dot", 
+              opacity=1, color="#D55E00") %>%
+    visGroups(groupname = "MN", shape = "dot", 
+              opacity=1, color="#cccccc")  %>%
+    addFontAwesome() %>%
+    visLegend(addNodes = list(
+      list(label = "66 syn", shape = "icon", 
+           icon = list(code = "f2d1", size = 30, color = "#D55E00"))), 
+      useGroups = TRUE,  width=0.1,ncol = 1,
+      position='right', stepY=70)
+  
+  
+  visNet
+}
 # create graph with cell types as nodes
 node_IDs <- data.frame(name = cell_types)
 graph.funct <- tbl_graph(nodes = node_IDs)
