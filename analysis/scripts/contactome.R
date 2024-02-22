6a7e346d06f0d5399f9b775b14dca8f1572d8ac7
Gaspar Jekely <g.jekely@exeter.ac.uk>
2024-02-22 15:50:11 +0100

    contactome: add tag query

--- analysis/scripts/contactome.R
+++ analysis/scripts/contactome.R
@@ -11,7 +11,68 @@ contacts <- catmaid_fetch(
 )
 length(contacts$connectors)
 
-skid1 <- contacts$partners[[1]][[1]][[3]]
+# get connector ids
+connector_ids <- c()
+for(i in 1:length(contacts$connectors)){
+  connector_ids[i] <- contacts$connectors[[i]][[1]]
+  }
+connector_ids
 
+partner1_skid <- c()
+for(i in 1:length(contacts$connectors)){
+  partner1_skid[i] <- contacts$partners[[i]][[1]][[3]]
+}
+partner1_skid
+
+partner2_skid <- c()
+for(i in 1:length(contacts$connectors)){
+  partner2_skid[i] <- contacts$partners[[i]][[2]][[3]]
+}
+partner2_skid
+
+node_id_of_contact_partner1 <- c()
+for(i in 1:length(contacts$connectors)){
+  node_id_of_contact_partner1[i] <- contacts$partners[[i]][[1]][[2]]
+}
+node_id_of_contact_partner1
+
+node_id_of_contact_partner2 <- c()
+for(i in 1:length(contacts$connectors)){
+  node_id_of_contact_partner2[i] <- contacts$partners[[i]][[2]][[2]]
+}
+node_id_of_contact_partner2
+
+
+
+connector_tb <- tibble(
+  connectors = (connector_ids),
+  skid1 = (partner1_skid),
+  skid2 = (partner2_skid)
+  )
+connector_tb
+
+#
+partner1_pre <- c()
+for(i in 1:length(contacts$connectors)){
+  neuron <- read.neuron.catmaid(partner1_skid[i], pid = 35)
+  partner1_pre[i] <- node_id_of_contact_partner1[i] %in% neuron$tags$enter_cil
+}
+partner1_pre
+node_id_of_contact_partner1[10]
+
+partner2_pre <- c()
+for(i in 1:length(contacts$connectors)){
+  neuron <- read.neuron.catmaid(partner2_skid[i], pid = 35)
+  partner2_pre[i] <- node_id_of_contact_partner2[i] %in% neuron$tags$enter_nocil
+}
+partner2_pre
+
+neuron <- read.neuron.catmaid(partner1_skid[1], pid = 35)
+neuron$tags$enter_cil
+node_id_of_contact_partner1[1] %in% neuron$tags$enter_cil
+
+neuron$`2476258`
+
+catmaid_t
 skid2 <- contacts$partners[[1]][[2]][[3]]
 
