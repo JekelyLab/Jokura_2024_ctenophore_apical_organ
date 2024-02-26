cdfcd4e32a8ff526bda8d2e5e37e99b896e6061c
Sanja Jasek <jasek.sanja@gmail.com>
2024-02-26 14:48:59 +0100

    add general function for cropping tagged locations in catmaid

--- analysis/scripts/packages_and_functions.R
+++ analysis/scripts/packages_and_functions.R
@@ -60,3 +60,109 @@ plot_background_ventral <- function(x){
   nview3d("ventral", extramat=rotationMatrix(0.9, 0.3, -0.1, 1))
   par3d(windowRect = c(0, 0, 800, 800)) #resize for frontal view
 }
+
+
+
+# crop from catmaid -----------------------------------------
+
+crop_catmaid <- function(tagname,
+                         half_bb_size_x, half_bb_size_y, half_bb_size_z,
+                         zoomlevel,
+                         dest_dir) {
+  # tag search only returns treenodes with tag, not connectors
+  # however, we want to get either treenodes or connectors, so we have to use generic search
+  tagname_edited <- gsub(" ", "%20", tagname)
+  search_results <- catmaid_fetch(path = paste("35/search?substring=", tagname_edited, sep = ""))
+  # search doesn't accept regex, so we have to filter results
+  ids=c() # keep track of the number of substacks we have to download from the server later
+          # use ids rather than simple counter because it helps with debugging
+  for (i in seq_along(search_results)) {
+    if (search_results[[i]]$name == tagname && search_results[[i]]$class_name == "label") {
+      for (j in seq_along(search_results[[i]]$nodes)) {
+        #print("adding treenode id")
+        #print(search_results[[i]]$nodes[[j]]$id)
+        ids <- c(ids, search_results[[i]]$nodes[[j]]$id)
+        x <- search_results[[i]]$nodes[[j]]$x
+        y <- search_results[[i]]$nodes[[j]]$y
+        z <- search_results[[i]]$nodes[[j]]$z
+        catmaid_fetch(
+          path = "35/crop/",
+          body = list(
+            stack_ids=28,
+            min_x=x-half_bb_size_x,
+            min_y=y-half_bb_size_y,
+            min_z=z-half_bb_size_z,
+            max_x=x+half_bb_size_x,
+            max_y=y+half_bb_size_y,
+            max_z=z+half_bb_size_z,
+            zoom_level=zoomlevel,
+            single_channel='true',
+            rotationcw=0
+          )
+        )
+      }
+      for (j in seq_along(search_results[[i]]$connectors)) {
+        #print("adding connector id")
+        #print(search_results[[i]]$connector[[j]]$id)
+        ids <- c(ids, search_results[[i]]$connectors[[j]]$id)
+        x <- search_results[[i]]$connectors[[j]]$x
+        y <- search_results[[i]]$connectors[[j]]$y
+        z <- search_results[[i]]$connectors[[j]]$z
+        catmaid_fetch(
+          path = "35/crop/",
+          body = list(
+            stack_ids=28,
+            min_x=x-half_bb_size_x,
+            min_y=y-half_bb_size_y,
+            min_z=z-half_bb_size_z,
+            max_x=x+half_bb_size_x,
+            max_y=y+half_bb_size_y,
+            max_z=z+half_bb_size_z,
+            zoom_level=zoomlevel,
+            single_channel='true',
+            rotationcw=0
+          )
+        )
+      }
+    }
+  }
+  print("checkpoint 1")
+  if (length(ids) == 0) {
+    print(paste("No treenodes or connectors tagged with", tagname, "were found"))
+    return()
+  }
+  
+  print(paste("Found", length(ids), "treenodes or connectors tagged with", tagname))
+  print("Downloading the following ids:")
+  print(ids)
+  print("This will take some time")
+        
+  # server needs time to process this
+  Sys.sleep(90)
+  
+  # download stacks ---------------------------------------------------------
+  # the exact file names are needed for download,
+  # to get them we need to check messages from the server
+  # originally I was checking which messages are new, and downloading those
+  # but this could lead to false positives
+  # so I decided that's it's probably safe to download the last n messages
+  # where n is the number of jobs
+  
+  # It would be cool if I can put the synapse ID as file name,
+  # but it's not guaranteed that jobs will return in the same order
+  
+  cat_messages <- catmaid_fetch(
+    path = "messages/list"
+  )
+  
+  for (k in seq_along(ids)) {
+    cat_message <- cat_messages[[k]]$text
+    link <- regmatches(cat_message, 
+                       regexpr("/crop/download/crop_.{6}.tiff", cat_message))
+    filename <- regmatches(link, 
+                           regexpr("crop_.{6}.tiff", link))
+    full_link <- paste("https:/catmaid.ex.ac.uk", link, sep = "")
+    destpath <- paste(dest_dir, "/", filename, sep = "")
+    download.file(full_link, destfile = destpath)
+  }
+}
