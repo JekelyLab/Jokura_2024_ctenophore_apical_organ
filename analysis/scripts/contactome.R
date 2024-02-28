91c1dc549cf490fdb4366ebebe36b6c3fd00ceb0
Gaspar Jekely <g.jekely@exeter.ac.uk>
2024-02-28 17:59:41 +0100

    contactome.R: added section to derive graph and convert to visnetwork

--- analysis/scripts/contactome.R
+++ analysis/scripts/contactome.R
@@ -140,9 +140,178 @@ time.taken
 
 connector_tb
 
-connector_tb |>
-  select(skid1, skid2) |>
-  pull()
 
-contactome_tbl_graph 
+# derive network from table -----------------
 
+
+connector_prepost_info_table
+
+#get all nodes (all skids)
+nodes1 <- connector_prepost_info_table |>
+  pull(partner1_skid)
+nodes2 <- connector_prepost_info_table |>
+  pull(partner2_skid)
+all_nodes <- unique(c(nodes1, nodes2))
+
+#edge lists
+edges1_to_2_from <- connector_prepost_info_table |>
+  filter(partner1_enter_cil == 1) |>
+  pull(partner1_skid) |> 
+  as.character()
+edges1_to_2_to <- connector_prepost_info_table |>
+  filter(partner1_enter_cil == 1) |>
+  pull(partner2_skid) |> 
+  as.character()
+edges2_to_1_from <- connector_prepost_info_table |>
+  filter(partner2_enter_cil == 1) |>
+  pull(partner2_skid) |> 
+  as.character()
+edges2_to_1_to <- connector_prepost_info_table |>
+  filter(partner2_enter_cil == 1) |>
+  pull(partner1_skid) |> 
+  as.character()
+edges2_to_1_from
+edges2_to_1_to
+
+# make graph
+cil_graph <- create_empty(0)
+
+#add nodes
+cil_graph <- cil_graph |> 
+  bind_nodes(as.data.frame(all_nodes))
+
+#add edges
+cil_graph <- cil_graph |> bind_edges(data.frame(from = edges1_to_2_from, to = edges1_to_2_to))
+cil_graph <- cil_graph |> bind_edges(data.frame(from = edges2_to_1_from, to = edges2_to_1_to))
+cil_graph
+
+# derive network method 2 ----------------
+
+connector_tb
+
+#get all nodes (all skids)
+nodes1 <- connector_tb |>
+  pull(skid1)
+nodes2 <- connector_tb |>
+  pull(skid2)
+all_nodes <- unique(c(nodes1, nodes2))
+
+#edge lists
+edges1_to_2_from <- connector_tb |>
+  filter(partner1_pre_cil == TRUE) |>
+  pull(skid1) |> 
+  as.character()
+edges1_to_2_from
+edges1_to_2_to <- connector_tb |>
+  filter(partner1_pre_cil == TRUE) |>
+  pull(skid2) |> 
+  as.character()
+edges1_to_2_to
+edges2_to_1_from <- connector_tb |>
+  filter(partner2_pre_cil == TRUE) |>
+  pull(skid2) |> 
+  as.character()
+edges2_to_1_from
+edges2_to_1_to <- connector_tb |>
+  filter(partner2_pre_cil == TRUE) |>
+  pull(skid1) |> 
+  as.character()
+edges2_to_1_to
+
+# make graph
+cil_graph2 <- create_empty(0, directed = TRUE)
+
+#add nodes
+cil_graph2 <- cil_graph |> 
+  bind_nodes(as.data.frame(all_nodes))
+
+#add edges
+cil_graph2 <- cil_graph |> bind_edges(data.frame(from = edges1_to_2_from, to = edges1_to_2_to))
+cil_graph2 <- cil_graph |> bind_edges(data.frame(from = edges2_to_1_from, to = edges2_to_1_to))
+cil_graph2
+
+# nocil graph -----------------------
+
+
+connector_prepost_info_table
+
+#get all nodes (all skids)
+nodes1 <- connector_prepost_info_table |>
+  pull(partner1_skid)
+nodes2 <- connector_prepost_info_table |>
+  pull(partner2_skid)
+all_nodes <- unique(c(nodes1, nodes2))
+
+neuron_names <- catmaid_get_neuronnames(all_nodes, pid = 35)
+
+#edge lists
+edges1_to_2_from <- connector_prepost_info_table |>
+  filter(partner1_enter_nocil == 1) |>
+  pull(partner1_skid) |> 
+  as.character()
+edges1_to_2_to <- connector_prepost_info_table |>
+  filter(partner1_enter_nocil == 1) |>
+  pull(partner2_skid) |> 
+  as.character()
+edges2_to_1_from <- connector_prepost_info_table |>
+  filter(partner2_enter_nocil == 1) |>
+  pull(partner2_skid) |> 
+  as.character()
+edges2_to_1_to <- connector_prepost_info_table |>
+  filter(partner2_enter_nocil == 1) |>
+  pull(partner1_skid) |> 
+  as.character()
+edges2_to_1_from
+edges2_to_1_to
+
+# make graph
+nocil_graph <- create_empty(0)
+
+#add nodes
+nocil_graph <- nocil_graph |> 
+  bind_nodes(as.data.frame(all_nodes))
+
+#add edges
+nocil_graph <- nocil_graph |> bind_edges(data.frame(from = edges1_to_2_from, to = edges1_to_2_to))
+nocil_graph <- nocil_graph |> bind_edges(data.frame(from = edges2_to_1_from, to = edges2_to_1_to))
+nocil_graph <- nocil_graph |>
+  mutate(name = unlist(neuron_names))
+nocil_graph <- nocil_graph |> activate(edges) |>
+  mutate(value = 1)
+
+nocil_graph
+
+as.igraph(nocil_graph)
+# graph plot --------------------------------------------------------------
+
+
+## convert to VisNetwork-list
+nocil_graph.visn <- toVisNetworkData(nocil_graph)
+nocil_graph.visn
+
+#define node color
+nocil_graph.visn$nodes$color <- sample(Okabe_Ito[1:8], 95, replace = TRUE)
+nocil_graph.visn$nodes$vakue <- 1
+
+#hierarchical layout
+visNetwork(nocil_graph.visn$nodes, nocil_graph.visn$edges) %>%
+  visIgraphLayout(
+    layout = "layout_nicely", physics = TRUE, 
+    randomSeed = 42, type="square"
+    ) %>%
+  visEdges(
+    smooth = list(type = 'curvedCW', roundness=0.2),
+    scaling=list(min=2, max=12),
+    color = list(inherit=TRUE, opacity=0.7),
+    arrows = list(
+      to = list(enabled = TRUE, 
+      scaleFactor = 1, type = 'arrow'))
+    ) %>%
+  visNodes(
+    borderWidth=0.3, 
+    color = list(background=nocil_graph.visn$nodes$color, border='black'),
+    opacity=0.9,
+    shape='dot', 
+    font=list(color='black', size=44),
+    scaling = list(label=list(enabled=TRUE, min=48, max=56))
+    )
