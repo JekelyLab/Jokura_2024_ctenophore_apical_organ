8f2ba09490c4cdf3002058793d61758d334d531c
Sanja Jasek <jasek.sanja@gmail.com>
2024-03-07 20:04:36 +0100

    cell type clustering: add missing func, neuron names
    - add neuron names to table of organelle counts
    - tag count function was accidentally deleted before original commit
      so script wasn't working. function is added now

--- analysis/scripts/cell_type_clustering.R
+++ analysis/scripts/cell_type_clustering.R
@@ -11,25 +11,38 @@ skids <- unlist(
 characters <- list("soma",
                    "centriole",
                    "basal body",
+                   "rootlets",
                    "golgi",
-                   "peroxisome?",
+                   "lysosome?",
                    "multivesicular body",
                    "SER",
                    "RER",
                    "plume",
                    "large clear vesicles",
                    "small clear vesicles",
+                   "vesicles",
                    "large electrondense vesicles",
-                   "small dark dots")
+                   "small dark dots",
+                   "cilium tip intracellular",
+                   "cilium tip inside different cell")
 
-tag_stats_per_skid <- data.frame(matrix(ncol = length(characters)+1, nrow = 0))
-colnames(tag_stats_per_skid) <- c("skid", characters)
+tag_stats_per_skid <- data.frame(matrix(ncol = length(characters)+2, nrow = 0))
+colnames(tag_stats_per_skid) <- c("skid", "neuron_name", characters)
+
+tag_count <- function(tagl, character) {
+  return(length(tagl[[character]]))
+}
+
 tag_stats <- function(skid) {
   # if skeleton has only one node read.neuron.catmaid gives an error
   try(neuron_info <- read.neuron.catmaid(skid, pid = 35), silent=TRUE)
   if("try-error" %in% class(t)) return(NULL)
   tags <- neuron_info$tags
-  counts <- mapply(tag_count, list(tags), characters)
+  
+  counts <- mapply(tag_count,
+                   list(tags), 
+                   characters)
+
   # ignore cells without a soma
   if (counts[[1]] == 0) {
     return(NULL)
@@ -40,13 +53,14 @@ tag_stats <- function(skid) {
 }
 
 for (skid in skids) {
+  neuron_name <- catmaid_get_neuronnames(skid, pid = 35)
   tag_counts <- tag_stats(skid)
   if (!is.null(tag_counts)) {
-    tag_stats_per_skid[nrow(tag_stats_per_skid) + 1,] = c(skid, tag_counts)
+    tag_stats_per_skid[nrow(tag_stats_per_skid) + 1,] = c(skid, neuron_name, tag_counts)
   }
 }
 
-tag_stats_noskid <- tag_stats_per_skid[,-1]
+tag_stats_noskid <- tag_stats_per_skid[,3:ncol(tag_stats_per_skid)]
 
 km <- kmeans(tag_stats_noskid, centers = 20, nstart = 25)
 fviz_cluster(km, data = tag_stats_per_skid)
@@ -82,4 +96,5 @@ for (i in seq(1:length(tag_stats_per_skid$skid))) {
   celltypes <- rbind(celltypes, new_row)
 }
 
-celltypes[order(celltypes[[3]]),]
+sorted <- celltypes[order(celltypes[[3]]),]
+
