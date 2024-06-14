b0b5c61411bb6499722962886f7a41431cfd42be
Sanja Jasek <jasek.sanja@gmail.com>
2024-06-14 19:07:15 +0200

    mito in syn: prototype figure

--- analysis/scripts/number_of_postsynaptic_sites_per_synapse.R
+++ analysis/scripts/number_of_postsynaptic_sites_per_synapse.R
@@ -1,8 +1,4 @@
-library(catmaid)
-library(data.table)
-library(tidyverse)
-
-conn <- source("~/R/conn.R")
+source("analysis/scripts/packages_and_functions.R")
 
 # calculate average number of post-synaptic sites per synapse  -----
 
@@ -168,7 +164,7 @@ mito_means_tidy <- mito_means %>%
 
 # plot mito vesicles by celltype -----------------------------------------------
 
-ggplot(mito_means_tidy, aes(
+plot_mito_stats <- ggplot(mito_means_tidy, aes(
   fill = factor(characteristic,
     levels = c("mean_vesicles_syn", "mean_vesicles_no_syn", "mean_vesicles_unclear", "mean_vesicles_none")
   ),
@@ -194,57 +190,120 @@ ggplot(mito_means_tidy, aes(
 
 
 
-# plot mitochondria positions --------------------------------------------------
+# plot mitochondria positions in SSN and bridge --------------------------------
+# I'm not sure it makes sense to do it for other celltypes because not all cells
+# for them had annotated mito
+for (cell_type in c("SSN", "bridge")) {
+  plot_background()
+  # plot3d(outline,
+  #  WithNodes = F, add = T, alpha = 0.07, col = "#E2E2E2"
+  # )
+  skids <- mito_stats %>%
+    filter(celltype == cell_type) %>%
+    select(skid) %>%
+    pull()
 
-plot_background()
-plot3d(neurons, color = "lightgrey", lwd = 2, soma = TRUE, alpha = 0.2)
+  for (skid in skids) {
+    cell <- mito_done[[as.character(skid)]] %>% smooth_neuron(sigma = 1000)
+    plot3d(cell, color = "grey", lwd = 2, soma = TRUE, alpha = 0.6)
+  }
 
-pos_ves_syn <- mito_vesicle_info |>
-  filter(celltype == "bridge" | celltype == "SSN") |>
-  filter(mito_type == "vesicles_syn") |>
-  select(x, y, z)
-plot3d(pos_ves_syn,
-  add = TRUE,
-  col = "#EE6677",
-  size = 5,
-  alpha = 1
-)
+  pos_ves_syn <- mito_vesicle_info |>
+    filter(celltype == cell_type) |>
+    filter(mito_type == "vesicles_syn") |>
+    select(x, y, z)
+  plot3d(pos_ves_syn,
+    add = TRUE,
+    col = "#EE6677",
+    size = 5,
+    alpha = 1
+  )
 
-pos_ves_no_syn <- mito_vesicle_info |>
-  filter(celltype == "bridge" | celltype == "SSN") |>
-  filter(mito_type == "vesicles_no_syn") |>
-  select(x, y, z)
-plot3d(pos_ves_no_syn,
-  add = TRUE,
-  col = "#DDAA33",
-  size = 5,
-  alpha = 1
-)
+  pos_ves_no_syn <- mito_vesicle_info |>
+    filter(celltype == cell_type) |>
+    filter(mito_type == "vesicles_no_syn") |>
+    select(x, y, z)
+  plot3d(pos_ves_no_syn,
+    add = TRUE,
+    col = "#DDAA33",
+    size = 5,
+    alpha = 1
+  )
 
-pos_ves_unc <- mito_vesicle_info |>
-  filter(celltype == "bridge" | celltype == "SSN") |>
-  filter(mito_type == "vesicles_unc") |>
-  select(x, y, z)
-plot3d(pos_ves_unc,
-  add = TRUE,
-  col = "#4477AA",
-  size = 5,
-  alpha = 1
-)
+  pos_ves_unc <- mito_vesicle_info |>
+    filter(celltype == cell_type) |>
+    filter(mito_type == "vesicles_unc") |>
+    select(x, y, z)
+  plot3d(pos_ves_unc,
+    add = TRUE,
+    col = "#4477AA",
+    size = 5,
+    alpha = 1
+  )
 
-pos_ves_no <- mito_vesicle_info |>
-  filter(celltype == "bridge" | celltype == "SSN") |>
-  filter(mito_type == "vesicles_none") |>
-  select(x, y, z)
-plot3d(pos_ves_no,
-  add = TRUE,
-  col = "black",
-  size = 5,
-  alpha = 1
-)
-
-# 
-if (!file.exists("manuscript/pictures/Figure_mito_syn_ves_syn.tif")) {
-  crop_substack("Figure_mito_syn_ves_syn", 700, 700, 0, 0, "manuscript/pictures", 35, 28)
-  
+  pos_ves_no <- mito_vesicle_info |>
+    filter(celltype == cell_type) |>
+    filter(mito_type == "vesicles_none") |>
+    select(x, y, z)
+  plot3d(pos_ves_no,
+    add = TRUE,
+    col = "black",
+    size = 5,
+    alpha = 1
+  )
+  anterior()
+  paste("manuscript/pictures/mito_pos_", cell_type, "_anterior.png", sep = "") %>% rgl.snapshot()
+  left()
+  paste("manuscript/pictures/mito_pos_", cell_type, "_left.png", sep = "") %>% rgl.snapshot()
+  close3d()
 }
+
+
+
+# TODO: automate renaming. Mybe make the crop funtion return a filename?
+if (!file.exists("manuscript/pictures/Figure_mito_syn_ves_syn.tiff")) {
+  crop_substack("Figure_mito_syn_ves_syn", 700, 700, 0, 0, "manuscript/pictures", 35, 28)
+}
+if (!file.exists("manuscript/pictures/Figure_mito_syn_ves_no_syn.tiff")) {
+  crop_substack("Figure_mito_syn_ves_no_syn", 700, 700, 0, 0, "manuscript/pictures", 35, 28)
+}
+if (!file.exists("manuscript/pictures/Figure_mito_syn_ves_unc.tiff")) {
+  crop_substack("Figure_mito_syn_ves_unc", 700, 700, 0, 0, "manuscript/pictures", 35, 28)
+}
+if (!file.exists("manuscript/pictures/Figure_mito_syn_ves_none.tiff")) {
+  crop_substack("Figure_mito_syn_ves_none", 700, 700, 0, 0, "manuscript/pictures", 35, 28)
+}
+
+
+EM_ves_syn <- magick::image_read("manuscript/pictures/Figure_mito_syn_ves_syn.tiff")
+EM_ves_no_syn <- magick::image_read("manuscript/pictures/Figure_mito_syn_ves_no_syn.tiff")
+EM_ves_unc <- magick::image_read("manuscript/pictures/Figure_mito_syn_ves_unc.tiff")
+EM_ves_none <- magick::image_read("manuscript/pictures/Figure_mito_syn_ves_none.tiff")
+
+panel_EM_ves_syn <- ggdraw() + draw_image(EM_ves_syn)
+panel_EM_ves_no_syn <- ggdraw() + draw_image(EM_ves_no_syn)
+panel_EM_ves_unc <- ggdraw() + draw_image(EM_ves_unc)
+panel_EM_ves_none <- ggdraw() + draw_image(EM_ves_none)
+
+mito_pos_SSN_anterior <- magick::image_read("manuscript/pictures/mito_pos_SSN_anterior.png")
+mito_pos_SSN_left <- magick::image_read("manuscript/pictures/mito_pos_SSN_left.png")
+mito_pos_bridge_anterior <- magick::image_read("manuscript/pictures/mito_pos_bridge_anterior.png")
+mito_pos_bridge_left <- magick::image_read("manuscript/pictures/mito_pos_bridge_left.png")
+
+panel_mito_pos_SSN_anterior <- ggdraw() + draw_image(mito_pos_SSN_anterior)
+panel_mito_pos_SSN_left <- ggdraw() + draw_image(mito_pos_SSN_left)
+panel_mito_pos_bridge_anterior <- ggdraw() + draw_image(mito_pos_bridge_anterior)
+panel_mito_pos_bridge_left <- ggdraw() + draw_image(mito_pos_bridge_left)
+
+
+layout <- "AABBCC
+           AADDEE
+           AAFGHI"
+
+figure_mito_in_syn <- plot_mito_stats +
+  panel_mito_pos_SSN_anterior + panel_mito_pos_SSN_left + panel_mito_pos_bridge_anterior +  panel_mito_pos_bridge_left +
+  panel_EM_ves_syn + panel_EM_ves_no_syn + panel_EM_ves_unc + panel_EM_ves_none +
+  plot_layout(design = layout) +
+  plot_annotation(tag_levels = "a") &
+  theme(plot.tag = element_text(size = 12, face = "plain"))
+figure_mito_in_syn
