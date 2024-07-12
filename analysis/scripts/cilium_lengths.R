40c5bb5a33012012cf059e5a461df1ca64d9b9d7
Sanja Jasek <jasek.sanja@gmail.com>
2024-07-12 17:37:14 +0200

    cilium lengths: new method of calculating cilia lenghts
    Uses segments_between tags function.

--- analysis/scripts/cilium_lengths.R
+++ analysis/scripts/cilium_lengths.R
@@ -102,3 +102,53 @@ for (neuroninf in neurons_info) {
 }
 summary(as.numeric(n_intra))
 
+
+ctip_skids <- tags[tags$labelName == "cilium tip", "skeletonID"] %>% unique()
+cil_neurons <- read.neurons.catmaid(ctip_skids, pid = 35, fetch.annotations = TRUE)
+annotations <- attr(cil_neurons, 'anndf') |> as.tibble()
+
+cilium_lengths <- tibble(celltype=character(),
+                         skid=character(),
+                         cil_length=integer(),
+                         pocket_length=integer())
+
+for (cell in cil_neurons) {
+  sigma = 2000
+  sskid <- cell$skid
+  # get celltype annotation
+  celltype <- annotations |> 
+    filter(skid==sskid) |> 
+    filter(grepl("celltype:", annotation)) |> 
+    select(annotation) |> 
+    pull()
+  celltype <- gsub(".*:","", celltype)
+  if (length(celltype)==0) {
+    celltype <- "NA"
+  }
+  # get cilium lengths
+  cilia <- segments_between_tags(cell, "cilium tip", "basal body")
+  for (cilium in cilia) {
+    cil_length <- smooth_neuron(cilium, sigma=sigma) %>%
+      summary() %>% select(cable.length)
+    # check if it has exit_ciliary_pocket tag
+    exit_cil_pocket <- cilium$tags[["exit_ciliary_pocket"]]
+    ctip_intra <- cilium$tags[["cilium tip instracellular"]]
+    if (length(exit_cil_pocket) > 0) {
+      pocket <- segments_between_tags(cilium, "exit_ciliary_pocket", "basal body")
+      pocket_length <- smooth_neuron(pocket, sigma = sigma) %>%
+        summary() %>% select(cable.length)
+    }
+    else if (length(ctip_intra) > 0) {
+      pocket_length <- cil_length
+    }
+    else {
+      pocket_length <- 0
+    }
+    cil_len_tb <- tibble(celltype=as.character(celltype),
+                         skid=as.character(sskid),
+                         cil_length=as.integer(cil_length),
+                         pocket_length=as.integer(pocket_length))
+    
+    cilium_lengths <- bind_rows(cilium_lengths, cil_len_tb)
+  }
+}
