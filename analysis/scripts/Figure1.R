e0f7239c4b83db0ff83f420c04bef30dbf658755
Gaspar Jekely <gaspar.jekely@cos.uni-heidelberg.de>
2025-02-13 16:32:42 +0100

    Fig1: fine tune

--- analysis/scripts/Figure1.R
+++ analysis/scripts/Figure1.R
@@ -187,30 +187,33 @@ panel_balancer <- ggdraw() + draw_image(readPNG("manuscript/pictures/balancer.pn
 panel_larva_pic <- ggdraw() + draw_image(image_read("manuscript/pictures/Mnemiopsis_larva_5dpf.png"))
 panel_AO_pic <- ggdraw() + draw_image(image_read("manuscript/pictures/AO_mag_pics.png"))
 panel_serial_sectioning <- ggdraw() + draw_image(readPNG("manuscript/pictures/serial sectioning.png"))
-panel_catmaid_overview <- ggdraw() + draw_image(readPNG("manuscript/pictures/overview.png"))
+panel_catmaid_overview <- ggdraw() + draw_image(readPNG("manuscript/pictures/overview.png")) +
+  draw_label("serial EM volume", x = 0.3, y = 0.98, size = 10, fontface="plain") +
+  draw_label("619 sections", x = 0.75, y = 0.2, color="black", size = 8, hjust = 0) +
+  draw_label("927 cells", x = 0.75, y = 0.1, color="black", size = 8, hjust = 0)
+
 panel_3d_all_cells <- ggdraw() + draw_image(readPNG("manuscript/pictures/all_cells_3_views.png"))
 panel_bal_ant <- ggdraw() + draw_image(readPNG("manuscript/pictures/balancer_Q1_4_aboral_view.png")) +
   draw_label("four quadrants", x = 0.4, y = 0.98, size = 10, fontface="plain", hjust = 0.5)
-  
 panel_bal_side <- ggdraw() + draw_image(readPNG("manuscript/pictures/balancer_Q1_4_side_view.png"))
 
 layout <- "
-AABBBBCCC
-#########
-DDEEEEEEE
+AAAAABBBBBBBBBBBBBBBBBB#CCCCCCCC
+################################
+DDDDDDDDEEEEEEEEEEEEEEEEEEEEEEEE
 "
 
 Figure1 <- panel_larva_pic + panel_AO_pic + panel_catmaid_overview + 
   panel_bal_ant + panel_balancer +
   plot_layout(
     design = layout,
-    heights = c(1,0.1,1.4)) + 
+    heights = c(1, 0.05, 1.4)) + 
   plot_annotation(tag_levels = "A") &
   theme(plot.tag = element_text(size = 12, face = "plain"))
 
 ggsave("manuscript/figures/Figure1.png", limitsize = FALSE, 
-       units = c("px"), Figure1, width = 2600, height = 1000, bg='white')  
+       units = c("px"), Figure1, width = 2800, height = 1200, bg='white')  
 
 ggsave("manuscript/figures/Figure1.pdf", limitsize = FALSE, 
-       units = c("px"), Figure1, width = 2600, height = 1000) 
+       units = c("px"), Figure1, width = 2400, height = 1000) 
 
