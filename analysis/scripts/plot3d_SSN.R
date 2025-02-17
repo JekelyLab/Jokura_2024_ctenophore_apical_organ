6d0e34281c142c29135223f2c155b7505812d75d
Kei Jokura <keijokura@gmail.com>
2025-02-17 17:23:49 +0900

    edit plot 3d SSN

--- analysis/scripts/plot3d_SSN.R
+++ analysis/scripts/plot3d_SSN.R
@@ -5,67 +5,50 @@ source("analysis/scripts/packages_and_functions.R")
 
 # load cell type ----------------------------------
 
-read_smooth_neuron <- function(annotation){
-  nlapply(read.neurons.catmaid(annotation, pid = 35),
-          function(x)
-            smooth_neuron(x, sigma = 1000))
-}
-
 balancer <- read_smooth_neuron("celltype:balancer")
 bridge <- read_smooth_neuron("celltype:bridge")
 bristle <- read_smooth_neuron("celltype:bristle")
-dome <- read_smooth_neuron("celltype:dome")
-Cgroove_tag <- read_smooth_neuron("celltype:Cgroove-tag")
 Cgroove_sag <- read_smooth_neuron("celltype:Cgroove-sag")
-intramulticilia <- read_smooth_neuron("celltype:intra-multi-ciliated")
+Cgroove_tag <- read_smooth_neuron("celltype:Cgroove-tag")
+dense_vesicle <- read_smooth_neuron("celltype:dense_vesicle")
+dome <- read_smooth_neuron("celltype:dome")
+epithelial_floor <- read_smooth_neuron("celltype:epithelial_floor")
+intra_multi_ciliated <- read_smooth_neuron("celltype:intra-multi-ciliated")
 lamellate <- read_smooth_neuron("celltype:lamellate")
 lithocyte <- read_smooth_neuron("celltype:lithocyte")
-neuron <- read_smooth_neuron("celltype:SSN")
 plumose <- read_smooth_neuron("celltype:plumose")
-dense_vesicle <- read_smooth_neuron("celltype:dense_vesicle")
-monocilia <- read_smooth_neuron("celltype:monociliated")
-bicilia <- read_smooth_neuron("celltype:biciliated")
-non_cilia <- read_smooth_neuron("celltype:nonciliated")
+SSN <- read_smooth_neuron("celltype:SSN")
 
+monociliated <- read_smooth_neuron("celltype:monociliated")
+biciliated <- read_smooth_neuron("celltype:biciliated")
+multiciliated <- read_smooth_neuron("celltype:multiciliated")
+nonciliated <- read_smooth_neuron("celltype:nonciliated")
 
-Q1 <- nlapply(read.neurons.catmaid("Q1", pid = 35),
-              function(x)
-                smooth_neuron(x, sigma = 1000))
-Q2 <- nlapply(read.neurons.catmaid("Q2", pid = 35),
-              function(x)
-                smooth_neuron(x, sigma = 1000))
-Q3 <- nlapply(read.neurons.catmaid("Q3", pid = 35),
-              function(x)
-                smooth_neuron(x, sigma = 1000))
-Q4 <- nlapply(read.neurons.catmaid("Q4", pid = 35),
-              function(x)
-                smooth_neuron(x, sigma = 1000))
+Q1 <- read_smooth_neuron("Q1")
+Q2 <- read_smooth_neuron("Q2")
+Q3 <- read_smooth_neuron("Q3")
+Q4 <- read_smooth_neuron("Q4")
 
-
-balancer_Q1 <- nlapply(read.neurons.catmaid(get_skids_with_annot(pid = 35, c("celltype:balancer", "Q1")),
-                                            pid = 35),
-                       function(x)
-                         smooth_neuron(x, sigma = 1000))
-balancer_Q2 <- nlapply(read.neurons.catmaid(get_skids_with_annot(pid = 35, c("celltype:balancer", "Q2")),
-                                            pid = 35),
-                       function(x)
-                         smooth_neuron(x, sigma = 1000))
-balancer_Q3 <- nlapply(read.neurons.catmaid(get_skids_with_annot(pid = 35, c("celltype:balancer", "Q3")),
-                                            pid = 35),
-                       function(x)
-                         smooth_neuron(x, sigma = 1000))
-balancer_Q4 <- nlapply(read.neurons.catmaid(get_skids_with_annot(pid = 35, c("celltype:balancer", "Q4")),
-                                            pid = 35),
-                       function(x)
-                         smooth_neuron(x, sigma = 1000))
-
-
-all_celltypes <- list(balancer, bridge, bristle, dome, Cgroove_tag, Cgroove_sag,
-                      intramulticilia, lamellate, lithocyte, neuron, 
-                      plumose, dense_vesicle, monocilia, bicilia, non_cilia)
-
+all_celltypes <- list(balancer,
+                      bridge,
+                      bristle,
+                      Cgroove_sag,
+                      Cgroove_tag,
+                      dense_vesicle,
+                      dome,
+                      epithelial_floor,
+                      intra_multi_ciliated,
+                      lamellate,
+                      lithocyte,
+                      plumose,
+                      SSN,
+                      monociliated,
+                      biciliated,
+                      multiciliated,
+                      nonciliated)
 
 # plot SSN neuron -----------------------------------------------
+
 
 plot_background()
 close3d()
@@ -77,49 +60,24 @@ mfrow3d(1, 3)  #defines the two scenes
 par3d(windowRect = c(0, 0, 2400, 700))
 
 #plot aboral view
-plot3d(neuron,
-       soma = TRUE, lwd = 1, add = T, alpha = 0.5, color = Okabe_Ito[c(1,5,8)],
+plot3d(SSN,
+       soma = T, lwd = 1, add = T, alpha = 0.5, col = Okabe_Ito[c(1, 5, 8)],
        WithConnectors = F, WithNodes = F)
 
-#for (object in objects) {
-#  plot3d(object, soma = TRUE, lwd = 1, add = TRUE, alpha = 0.05, col = Okabe_Ito[8])
-#}
 
-
-plot3d(balancer, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(bridge, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(bristle, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(dome, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(Cgroove_tag, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(Cgroove_sag, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(intramulticilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(lamellate, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(lithocyte, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(plumose, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(dense_vesicle, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(monocilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(bicilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(non_cilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
+for (i in 1:length(all_celltypes)) {
+  print(i)
+  plot3d(
+    all_celltypes[[i]], soma = TRUE, lwd = 1, add = TRUE, 
+    alpha = 0.05, col = Okabe_Ito[8]
+  )
+}
 
 #texts3d(58000,51000,5000, "INRGW", cex = 3, col = "#56B4E9")
 
 #aboral view
 nview3d("anterior", extramat = rotationMatrix(1.05, 250, -200, 1000))
-#rgl.snapshot("manuscript/pictures/balancer_aboral_view.png")
+#rgl.snapshot("manuscript/pictures/SSN_aboral_view.png")
 par3d(zoom=0.61)
 
 #move to next panel in rgl window
@@ -128,48 +86,22 @@ next3d(clear=F)
 
 
 #plot lateral view of Sagittal plane
-plot3d(neuron,
-       soma = TRUE, lwd = 1, add = T, alpha = 0.5, color = Okabe_Ito[c(1,5,8)],
+plot3d(SSN,
+       soma = T, lwd = 1, add = T, alpha = 0.5, col = Okabe_Ito[c(1, 5, 8)],
        WithConnectors = F, WithNodes = F)
 
-#for (object in objects) {
-#  plot3d(object, soma = TRUE, lwd = 1, add = TRUE, alpha = 0.05, col = Okabe_Ito[8])
-#}
-
-plot3d(balancer, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(bridge, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(bristle, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(dome, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(Cgroove_tag, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(Cgroove_sag, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(intramulticilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(lamellate, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(lithocyte, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(plumose, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(dense_vesicle, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(monocilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(bicilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(non_cilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-
+for (i in 1:length(all_celltypes)) {
+  print(i)
+  plot3d(
+    all_celltypes[[i]], soma = TRUE, lwd = 1, add = TRUE, 
+    alpha = 0.05, col = Okabe_Ito[8]
+  )
+}
 
 
 
 nview3d("left", extramat = rotationMatrix(300, 4200, 1800, 800))
-#rgl.snapshot("manuscript/pictures/balancer_sagittal_plane.png")
+#rgl.snapshot("manuscript/pictures/SSN_sagittal_plane.png")
 par3d(zoom=0.61)
 
 #move to next panel in rgl window
@@ -178,47 +110,21 @@ next3d(clear=F)
 
 
 #plot lateral view of Tentacular plane
-plot3d(neuron,
-       soma = TRUE, lwd = 1, add = T, alpha = 0.5, color = Okabe_Ito[c(1,5,8)],
+plot3d(SSN,
+       soma = T, lwd = 1, add = T, alpha = 0.5, col = Okabe_Ito[c(1, 5, 8)],
        WithConnectors = F, WithNodes = F)
 
-#for (object in objects) {
-#  plot3d(object, soma = TRUE, lwd = 1, add = TRUE, alpha = 0.05, col = Okabe_Ito[8])
-#}
-
-plot3d(balancer, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(bridge, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(bristle, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(dome, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(Cgroove_tag, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(Cgroove_sag, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(intramulticilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(lamellate, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(lithocyte, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(plumose, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(dense_vesicle, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(monocilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(bicilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-plot3d(non_cilia, 
-       soma = T, lwd = 1, add = T, alpha = 0.05, col = Okabe_Ito[8])
-
+for (i in 1:length(all_celltypes)) {
+  print(i)
+  plot3d(
+    all_celltypes[[i]], soma = TRUE, lwd = 1, add = TRUE, 
+    alpha = 0.05, col = Okabe_Ito[8]
+  )
+}
 
 
 nview3d("left", extramat = rotationMatrix(-1.7, 190, -120, -140))
-#rgl.snapshot("manuscript/pictures/balancer_tentacular_plane.png")
+#rgl.snapshot("manuscript/pictures/SSN_tentacular_plane.png")
 par3d(zoom=0.61)
 
 #move to next panel in rgl window
