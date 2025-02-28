136e89d2182a5f9ae6c6416cc1da77cb022fe608
Kei Jokura <keijokura@gmail.com>
2025-02-28 20:16:13 +0900

    edit Figure1

--- analysis/scripts/Figure1.R
+++ analysis/scripts/Figure1.R
@@ -13,20 +13,31 @@ Q4 <- read_smooth_neuron("Q4")
 with_soma <- read_smooth_neuron("with_soma")
 lithocyte <- read_smooth_neuron("celltype:lithocyte")
 
-# plot balancer and quadrants ----------------------
+# plot quadrants ----------------------
 
 plot_background()
+close3d()
+# 3d plotting of cells
+nopen3d() 
+mfrow3d(1, 3)  #defines the two scenes
+#define the size of the rgl window, the view and zoom
+#par3d(windowRect = c(0, 0, 1200, 350))
+par3d(windowRect = c(0, 0, 2400, 700))
 
-plot3d(
-  balancer,
-  soma = T, lwd = 1, add = T,
-  alpha = 0.9, col = Okabe_Ito[5],
-  WithConnectors = F
-)
+#plot aboral view
+
+
+
+#plot3d(
+#  balancer,
+#  soma = T, lwd = 1, add = T,
+#  alpha = 0.9, col = Okabe_Ito[5],
+#  WithConnectors = F
+#)
 plot3d(
   lithocyte,
   soma = T, lwd = 1, add = T,
-  alpha = 0.8, col = Okabe_Ito[8],
+  alpha = 0.5, col = Okabe_Ito[8],
   WithConnectors = F
 )
 plot3d(
@@ -50,42 +61,139 @@ plot3d(
   alpha = 0.15, col = Okabe_Ito[7]
 )
 
-plot3d(
-  outline,
-  add = T,
-  alpha = 0.06, col = "grey50"
-)
-plot3d(
-  dome_cavity,
-  add = T,
-  alpha = 0.1, col = "grey50"
-)
+#plot3d(
+#  outline,
+#  add = T,
+#  alpha = 0.06, col = "grey50"
+#)
 
 
 nview3d("anterior", extramat = rotationMatrix(1.05, 250, -200, 1000))
 
+
+
 text3d(15402, 56436, -200, "lithocyte", cex = 2.5)
-text3d(38002, 65436, 10000, "balancer", cex = 2.5)
+#text3d(38002, 65436, 10000, "balancer", cex = 2.5)
 text3d(23402, 29436, -200, "Q1", cex = 2.5)
 text3d(11602, 54436, -200, "Q2", cex = 2.5)
 text3d(23402, 65436, -200, "Q3", cex = 2.5)
 text3d(36402, 35436, -200, "Q4", cex = 2.5)
 
-# aboral view
-rgl.snapshot("manuscript/pictures/balancer_Q1_4_aboral_view.png")
 
-# side view
+#rgl.snapshot("manuscript/pictures/quadrants_aboral_view.png")
+par3d(zoom=0.61)
+
+#move to next panel in rgl window
+next3d(clear=F)
+
+
+
+#plot lateral view of Sagittal plane
+
+
+plot3d(
+  lithocyte,
+  soma = T, lwd = 1, add = T,
+  alpha = 0.5, col = Okabe_Ito[8],
+  WithConnectors = F
+)
+plot3d(
+  Q1,
+  soma = T, lwd = 1, add = T,
+  alpha = 0.15, col = Okabe_Ito[1]
+)
+plot3d(
+  Q2,
+  soma = T, lwd = 1, add = T,
+  alpha = 0.15, col = Okabe_Ito[2]
+)
+plot3d(
+  Q3,
+  soma = T, lwd = 1, add = T,
+  alpha = 0.15, col = Okabe_Ito[6]
+)
+plot3d(
+  Q4,
+  soma = T, lwd = 1, add = T,
+  alpha = 0.15, col = Okabe_Ito[7]
+)
+
+#plot3d(
+#  outline,
+#  add = T,
+#  alpha = 0.06, col = "grey50"
+#)
+
+
 nview3d("left", extramat = rotationMatrix(300, 4200, 1800, 800))
-rgl.pop()
-rgl.pop()
-rgl.pop()
-rgl.pop()
-rgl.pop()
-text3d(41002, 65436, 18000, "balancer", cex = 2)
 
-rgl.snapshot("manuscript/pictures/balancer_Q1_4_side_view.png")
+text3d(23402, 29436, -200, "Q1", cex = 2.5)
+text3d(23402, 29436, 2000, "Q4", cex = 2.5)
+text3d(33402, 70436, -7000, "Q2", cex = 2.5)
+text3d(33402, 70436, -5000, "Q3", cex = 2.5)
+
+#rgl.snapshot("manuscript/pictures/quadrants_viewing_sagittal_plane.png")
+par3d(zoom=0.61)
+
+#move to next panel in rgl window
+next3d(clear=F)
+
+
+
+#plot lateral view of Tentacular plane
+
+plot3d(
+  lithocyte,
+  soma = T, lwd = 1, add = T,
+  alpha = 0.5, col = Okabe_Ito[8],
+  WithConnectors = F
+)
+plot3d(
+  Q1,
+  soma = T, lwd = 1, add = T,
+  alpha = 0.15, col = Okabe_Ito[1]
+)
+plot3d(
+  Q2,
+  soma = T, lwd = 1, add = T,
+  alpha = 0.15, col = Okabe_Ito[2]
+)
+plot3d(
+  Q3,
+  soma = T, lwd = 1, add = T,
+  alpha = 0.15, col = Okabe_Ito[6]
+)
+plot3d(
+  Q4,
+  soma = T, lwd = 1, add = T,
+  alpha = 0.15, col = Okabe_Ito[7]
+)
+
+#plot3d(
+#  outline,
+#  add = T,
+#  alpha = 0.06, col = "grey50"
+#)
+
+#plot lateral view of Tentacular plane
+nview3d("left", extramat = rotationMatrix(-1.7, 190, -120, -140))
+
+text3d(23402, 25436, -200, "Q1", cex = 2.5)
+text3d(23402, 25436, 2000, "Q2", cex = 2.5)
+text3d(33402, 50436, -4000, "Q3", cex = 2.5)
+text3d(33402, 50436, -2000, "Q4", cex = 2.5)
+
+#rgl.snapshot("manuscript/pictures/quadrants_viewing_tentacular_plane.png")
+par3d(zoom=0.61)
+
+#make a snapshot to the working directory
+rgl.snapshot("manuscript/pictures/quadrants.png")
 
 close3d()
+
+
+
+
 
 # plot balancer -----------------------------------------------
 
@@ -217,22 +325,32 @@ panel_AO_pic <- ggdraw() + draw_image(image_read("manuscript/pictures/AO_mag_pic
   draw_line(x = c(0.89, 0.99), y = c(0.05, 0.05), color = "black", size = 0.5) +
   draw_label(expression(paste("20 ", mu, " m")), x = 0.94, y = 0.09, color = "black", size = 7, hjust = 0.5)
 
+panel_larva_schematic <- ggdraw() + draw_image(image_read("manuscript/pictures/larva_aboral_view_schematic.png")) +
+  draw_label("aboral view", x = 0.5, y = 0.97, color = "black", size = 8) 
+
+panel_AO_schematic <- ggdraw() + draw_image(image_read("manuscript/pictures/AO_schematic.png")) +
+  draw_label("aboral view", x = 0.25, y = 0.97, color = "black", size = 8) +
+  draw_label("lateral view of SA plane", x = 0.5, y = 0.97, color = "black", size = 8) +
+  draw_label("lateral view of TA plane", x = 0.75, y = 0.97, color = "black", size = 8)
+
+
 panel_catmaid_overview <- ggdraw() + draw_image(readPNG("manuscript/pictures/overview.png")) +
   draw_label("serial EM volume", x = 0.3, y = 0.98, size = 10, fontface = "plain") +
   draw_label("619 sections", x = 0.05, y = 0.9, color = "black", size = 8, hjust = 0) +
   draw_label("927 cells", x = 0.05, y = 0.8, color = "black", size = 8, hjust = 0)
 
-panel_schematic <- ggdraw() + draw_image(image_read("manuscript/pictures/AO_schematic.png")) +
-  draw_label("aboral view", x = 0.27, y = 0.97, color = "black", size = 8) +
-  draw_label("lateral view of SA plane", x = 0.6, y = 0.97, color = "black", size = 8) +
-  draw_label("lateral view of TA plane", x = 0.85, y = 0.97, color = "black", size = 8)
-
 #panel_3d_all_cells <- ggdraw() + draw_image(readPNG("manuscript/pictures/all_cells_3_views.png"))
 
-panel_bal_ant <- ggdraw() + draw_image(readPNG("manuscript/pictures/balancer_Q1_4_aboral_view.png")) +
-  draw_label("four quadrants", x = 0.4, y = 0.98, size = 10, fontface = "plain", hjust = 0.5)
+#panel_bal_ant <- ggdraw() + draw_image(readPNG("manuscript/pictures/balancer_Q1_4_aboral_view.png")) +
+#  draw_label("four quadrants", x = 0.4, y = 0.98, size = 10, fontface = "plain", hjust = 0.5)
 
-panel_bal_side <- ggdraw() + draw_image(readPNG("manuscript/pictures/balancer_Q1_4_side_view.png"))
+#panel_bal_side <- ggdraw() + draw_image(readPNG("manuscript/pictures/balancer_Q1_4_side_view.png"))
+
+panel_quadrants <- ggdraw() + draw_image(readPNG("manuscript/pictures/quadrants.png")) +
+  draw_label("aboral view", x = 0.25, y = 0.97, color = "black", size = 8) +
+  draw_label("lateral view of SA plane", x = 0.5, y = 0.97, color = "black", size = 8) +
+  draw_label("lateral view of TA plane", x = 0.75, y = 0.97, color = "black", size = 8)
+
 
 # create panel A, B, create separately for precise alignment
 #Fig1A_B <- plot_grid(panel_larva_pic, NULL, panel_AO_pic,
@@ -270,23 +388,26 @@ panel_bal_side <- ggdraw() + draw_image(readPNG("manuscript/pictures/balancer_Q1
 
 
 layout <- "
-A#BBB#CC
-########
-DDDDD#EF
+A#BBB
+#####
+C#DDD
+#####
+E#FFF
 "
 
-Figure1 <- panel_larva_pic + panel_AO_pic + panel_catmaid_overview +
-  panel_schematic + panel_bal_ant + panel_bal_side +
+Figure1 <- panel_larva_pic + panel_AO_pic + 
+  panel_larva_schematic + panel_AO_schematic + 
+  panel_catmaid_overview + panel_quadrants +
   plot_layout(
     design = layout,
-    heights = c(1, 0.05, 1),
-    widths = c(1, 0.1, 1.1, 1.1, 1.1, 0.1, 0.8, 0.8)
+    heights = c(1, 0.1, 1, 0.1, 1),
+    widths = c(1, 0.1, 1, 1, 1)
   ) +
   plot_annotation(tag_levels = "A")
 
 ggsave("manuscript/figures/Figure1.png",
   limitsize = FALSE,
-  units = c("px"), Figure1, width = 3200, height = 1250, bg = "white"
+  units = c("px"), Figure1, width = 3200, height = 2250, bg = "white"
 )
 
 ggsave("manuscript/figures/Figure1.pdf",
