b4ad1c2ab09d960c27104e0363ddb6f5f505b751
Kei Jokura <keijokura@gmail.com>
2025-03-03 18:18:37 +0900

    edit Figure

--- analysis/scripts/Figure2.R
+++ analysis/scripts/Figure2.R
@@ -4,10 +4,6 @@
 source("analysis/scripts/packages_and_functions.R")
 
 
-
-
-
-
 # load cell type ----------------------------------
 
 balancer <- read_smooth_neuron("celltype:balancer")
@@ -51,6 +47,8 @@ all_celltypes <- list(balancer,
                       biciliated,
                       multiciliated,
                       nonciliated)
+
+
 
 # plot SSN neuron -----------------------------------------------
 
@@ -146,6 +144,162 @@ close3d()
 
 
 
+# retrieve connectors ----------------------------------------------------------
+
+conn_syn <- connectors(SSN)
+presyn_syn <- subset(conn_syn, prepost == 0)
+postsyn_syn <- subset(conn_syn, prepost == 1)
+
+
+
+
+# plot SSN synapses ---------------------------------------------------------------------
+
+plot_background()
+close3d()
+# 3d plotting of cells
+nopen3d() 
+mfrow3d(1, 3)  #defines the two scenes
+#define the size of the rgl window, the view and zoom
+#par3d(windowRect = c(0, 0, 1200, 350))
+par3d(windowRect = c(0, 0, 2400, 700))
+
+#plot aboral view
+plot3d(SSN,
+       soma = FALSE, lwd = 2, add = TRUE, alpha = 0.5, 
+       col = Okabe_Ito[c(1, 5, 8)],
+       WithConnectors = F, WithNodes = F)
+
+# plot presynapses
+plot3d(
+  presyn_syn$x, 
+  presyn_syn$y, 
+  presyn_syn$z, 
+  size = 5, alpha = 1, col = "magenta2", 
+  add = TRUE,
+  point_antialias = TRUE
+)
+
+# plot postsynapses
+plot3d(
+  postsyn_syn$x, 
+  postsyn_syn$y, 
+  postsyn_syn$z, 
+  size = 5, alpha = 1, col = "cyan2", 
+  add = TRUE,
+  point_antialias = TRUE
+)
+
+for (i in 1:length(all_celltypes)) {
+  print(i)
+  plot3d(
+    all_celltypes[[i]], soma = TRUE, lwd = 1, add = TRUE, 
+    alpha = 0.05, col = Okabe_Ito[8]
+  )
+}
+
+#texts3d(58000,51000,5000, "INRGW", cex = 3, col = "#56B4E9")
+
+#aboral view
+nview3d("anterior", extramat = rotationMatrix(1.05, 250, -200, 1000))
+#rgl.snapshot("manuscript/pictures/balancer_aboral_view.png")
+par3d(zoom=0.61)
+
+#move to next panel in rgl window
+next3d(clear=F)
+
+
+
+
+#plot lateral view of Sagittal plane
+plot3d(SSN,
+       soma = FALSE, lwd = 2, add = TRUE, alpha = 0.5, 
+       col = Okabe_Ito[c(1, 5, 8)],
+       WithConnectors = F, WithNodes = F)
+
+# plot presynapses
+plot3d(
+  presyn_syn$x, 
+  presyn_syn$y, 
+  presyn_syn$z, 
+  size = 5, alpha = 1, col = "magenta2", 
+  add = TRUE,
+  point_antialias = TRUE
+)
+
+# plot postsynapses
+plot3d(
+  postsyn_syn$x, 
+  postsyn_syn$y, 
+  postsyn_syn$z, 
+  size = 5, alpha = 1, col = "cyan2", 
+  add = TRUE,
+  point_antialias = TRUE
+)
+
+for (i in 1:length(all_celltypes)) {
+  print(i)
+  plot3d(
+    all_celltypes[[i]], soma = TRUE, lwd = 1, add = TRUE, 
+    alpha = 0.05, col = Okabe_Ito[8]
+  )
+}
+
+nview3d("left", extramat = rotationMatrix(300, 4200, 1800, 800))
+#rgl.snapshot("manuscript/pictures/balancer_sagittal_plane.png")
+par3d(zoom=0.61)
+
+#move to next panel in rgl window
+next3d(clear=F)
+
+
+
+#plot lateral view of Tentacular plane
+plot3d(SSN,
+       soma = FALSE, lwd = 2, add = TRUE, alpha = 0.5, 
+       col = Okabe_Ito[c(1, 5, 8)],
+       WithConnectors = F, WithNodes = F)
+
+# plot presynapses
+plot3d(
+  presyn_syn$x, 
+  presyn_syn$y, 
+  presyn_syn$z, 
+  size = 5, alpha = 1, col = "magenta2", 
+  add = TRUE,
+  point_antialias = TRUE
+)
+
+# plot postsynapses
+plot3d(
+  postsyn_syn$x, 
+  postsyn_syn$y, 
+  postsyn_syn$z, 
+  size = 5, alpha = 1, col = "cyan2", 
+  add = TRUE,
+  point_antialias = TRUE
+)
+
+for (i in 1:length(all_celltypes)) {
+  print(i)
+  plot3d(
+    all_celltypes[[i]], soma = TRUE, lwd = 1, add = TRUE, 
+    alpha = 0.05, col = Okabe_Ito[8]
+  )
+}
+
+nview3d("left", extramat = rotationMatrix(-1.7, 190, -120, -140))
+#rgl.snapshot("manuscript/pictures/balancer_tentacular_plane.png")
+par3d(zoom=0.61)
+
+#move to next panel in rgl window
+next3d(clear=F)
+
+
+#make a snapshot to the working directory
+rgl.snapshot("manuscript/pictures/SSN_prepost_synapse.png")
+
+close3d()
 
 
 
@@ -159,32 +313,41 @@ close3d()
 
 # assemble figure -------------------------------------------------------------
 
+panel_mito_in_syn_assemble <- ggdraw() + draw_image(readPNG("manuscript/pictures/mito_in_syn_assemble.png"))
+panel_mito_pos_SSN_anterior_trim <- ggdraw() + draw_image(readPNG("manuscript/pictures/mito_pos_SSN_anterior_trim.png"))
+panel_mito_pos_SSN_left <- ggdraw() + draw_image(readPNG("manuscript/pictures/mito_pos_SSN_left.png"))
 
-panel_synapse <- ggdraw() + draw_image(readPNG("manuscript/pictures/Figure_mito_syn_ves_syn.png"))
+
 panel_SSN_Q1234 <- ggdraw() + draw_image(readPNG("manuscript/pictures/SSN_Q1234_from_CATMAID.png"))
 panel_SSN_Q12_Q34 <- ggdraw() + draw_image(readPNG("manuscript/pictures/SSN_Q12_Q34_from_CATMAID.png"))
-panel_mito <- ggdraw() + draw_image(readPNG("manuscript/pictures/mito_pos_SSN_anterior_trim.png"))
+
+panel_synapse <- ggdraw() + draw_image(readPNG("manuscript/pictures/Figure_mito_syn_ves_syn.png"))
+
+panel_SSN_prepost_synapse <- ggdraw() + draw_image(readPNG("manuscript/pictures/SSN_prepost_synapse.png"))
 
 layout <- "
-AA#BBB
+AABCDD
 ######
-DD#CCC
+EEEFFF
+######
+GGGGHH
 "
 
-Figure2 <- panel_synapse + 
+Figure2 <- panel_mito_in_syn_assemble + panel_mito_pos_SSN_anterior_trim + panel_mito_pos_SSN_anterior_trim + panel_mito_pos_SSN_left +
   panel_SSN_Q1234 + panel_SSN_Q12_Q34 +
-  panel_mito +
+  panel_SSN_prepost_synapse + panel_synapse + 
   plot_layout(design = layout,
-              heights = c(1, 0.1, 1),
-              widths = c(1, 1, 0.1, 1, 1, 1)) + 
-  plot_annotation(tag_levels = "A") + 
-  theme(plot.tag = element_text(size = 12, face='plain', color='black'))
+              heights = c(1.25, 0.1, 1, 0.1, 1),
+              widths = c(1, 1, 1, 1, 1, 1)) + 
+  patchwork::plot_annotation(tag_levels = "A") &  
+  ggplot2::theme(plot.tag = element_text(size = 12, 
+                                         face='plain', color='black'))
 
 
 ggsave("manuscript/figures/Figure2.png", limitsize = FALSE, 
-       units = c("px"), Figure2, width = 2400, height = 1000, bg='white')  
+       units = c("px"), Figure2, width = 2800, height = 2000, bg='white')  
 
 
 ggsave("manuscript/figures/Figure2.pdf", limitsize = FALSE, 
-       units = c("px"), Figure2, width = 2400, height = 1000) 
+       units = c("px"), Figure2, width = 2800, height = 2000) 
 
