19458ebb0ea4fc42f05dce508539bbe6c6165085
Kei Jokura <keijokura@gmail.com>
2025-04-17 12:02:36 +0900

    edit figure 4

--- analysis/scripts/Figure4.R
+++ analysis/scripts/Figure4.R
@@ -13,8 +13,8 @@ df_time_dif <- read_csv("analysis/data/arrest_rebeat_time_differences.csv")
 
 plot_arrest_rebeat <- 
   ggplot(df_time_dif) +
-  aes(x = response, y = time, fill = response) +  
-  geom_boxplot(size = 0.5, outlier.shape = NA) +
+  aes(x = response, y = time, fill = response, color = response) +  
+  geom_boxplot(aes(color = plane), size = 0.5, outlier.shape = NA) +
   geom_beeswarm(aes(color = time),
                 size = 2,
                 cex = 3.2,
@@ -34,8 +34,10 @@ plot_arrest_rebeat <-
     axis.ticks.x = element_blank()  
   ) +
   scale_fill_manual(
-    values = c("re-beat" = "#28A8FF", 
-               "arrest" = "#f56EBA")) +
+    values = c("re-beat" = "gray", 
+               "arrest" = "white")) +
+  scale_color_manual(
+    values = c("sagittal" = "#0097b7", "tentacular" = "#f39500")) +
   facet_grid(~ plane, scales = "free_x", space = "free_x", switch = "x")  
 
 
@@ -55,33 +57,44 @@ ggsave(
 # assemble figure -------------------------------------------------------------
 
 panel_ms <- ggdraw() + draw_image(readPNG("manuscript/pictures/tilt_microscope.png"))
+panel_balancer <- ggdraw() + draw_image(readPNG("manuscript/pictures/balancer_closeup.png")) +
+  draw_label("sagittal", x = 0.25, y = 0.99, size = 7, hjust = 0.5) +
+  draw_label("tentacular", x = 0.75, y = 0.99, size = 7, hjust = 0.5)
 
 panel_kymograph <- ggdraw() + draw_image(readPNG("manuscript/pictures/balacer_arrest_rebeat_kymograph.png"))
-
 panel_graph <- ggdraw() + draw_image(readPNG("manuscript/pictures/arrest_rebeat_graph.png"))
+
+panel_cbf_bar_sag <- ggdraw() + draw_image(readPNG("analysis/data/balancer_CBF_Pearson_correlation_analysis/output/CBF_barplot/CBF_barplot_23_08_04_WT1_030_Rec_at_100.73fps_dark.csv.png")) +
+  draw_label("sagittal", x = 0.5, y = 0.99, size = 7, hjust = 0.5)
+panel_cbf_bar_tent <- ggdraw() + draw_image(readPNG("analysis/data/balancer_CBF_Pearson_correlation_analysis/output/CBF_barplot/CBF_barplot_23_08_01_WT1_007_Rec_at_100.20fps_dark.csv.png")) +
+  draw_label("tentacular", x = 0.5, y = 0.99, size = 7, hjust = 0.5)
+panel_cor_graph <- ggdraw() + draw_image(readPNG("analysis/data/balancer_CBF_Pearson_correlation_analysis/output/pearson_boxplot/boxplot_correlation_angle_0-20_win_20.png"))
 
 panel_comparison <- ggdraw() + draw_image(readPNG("manuscript/pictures/map_comparison.png"))
 
 layout <- "
 ABB
 ###
-CDD
+CCD
+###
+EFG
 "
 
-Figure4 <- panel_ms + panel_kymograph + 
-  panel_graph + panel_comparison +
+Figure4 <- panel_ms + panel_balancer + 
+  panel_kymograph + panel_graph + 
+  panel_cbf_bar_sag + panel_cbf_bar_tent + panel_cor_graph +
   plot_layout(design = layout,
-              heights = c(1, 0.1, 1),
+              heights = c(1, 0.1, 1, 0.1, 1),
               widths = c(1, 1, 1)) + 
   patchwork::plot_annotation(tag_levels = "A") &  
   ggplot2::theme(plot.tag = element_text(size = 12, 
                                          face='plain', color='black'))
 
 ggsave("manuscript/figures/Figure4.png", limitsize = FALSE, 
-       units = c("px"), Figure4, width = 2300, height = 1500, bg='white')  
+       units = c("px"), Figure4, width = 2000, height = 2000, bg='white')  
 
 
 ggsave("manuscript/figures/Figure4.pdf", limitsize = FALSE, 
-       units = c("px"), Figure4, width = 2300, height = 1500) 
+       units = c("px"), Figure4, width = 2000, height = 2000) 
 
 
